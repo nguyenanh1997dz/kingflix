ed0bc64a0c22ad5bec164c97c60a77fb78ad610b
nguyenanh1997dz <nguyenanh1997dz@gmail.com>
2024-06-07 14:27:55 +0700

    Không sửa gì

--- client/src/Screen/Login.jsx
+++ client/src/Screen/Login.jsx
@@ -107,9 +107,7 @@ const Login = () => {
           </p>
           <button
             type="button"
-            onClick={() => {
-              alert("Please enter your")
-            }}
+            onClick={googleAuth}
             className="text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 mr-2 mb-2"
           >
             <FaGoogle className="mr-2 -ml-1 w-4 h-4" />
